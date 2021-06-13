d383357f0cc6ee072bc91864dfb4963f6f080af1
Mathieu Walravens <m.walravens@students.ephec.be>
2021-06-13 02:49:52 +0200

    Fix Handshake's server port

--- mcli/packets/send/handshaking/handshake.py
+++ mcli/packets/send/handshaking/handshake.py
@@ -1,10 +1,10 @@
 from mcli.packets import Packet
-from mcli.packets.types import varint
+from mcli.packets.types import varint, ushort
 
 
 class Handshake(Packet, id=0x00):
     protocol_version: varint
     server_address: str
-    server_port: varint
+    server_port: ushort
     next_state: varint
 
