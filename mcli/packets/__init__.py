6021216f8942a4d75237d0e9b0b2d92b369492cb
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2022-05-30 19:45:34 +0200

    rename import

--- mcli/packets/__init__.py
+++ mcli/packets/__init__.py
@@ -3,4 +3,4 @@ from mcli.packets.packet import Packet, ReadPacket, WritePacket
 # Import packets after the initialization of the packet module
 from mcli.packets import clientbound, serverbound  # isort: split
 
-__all__ = ['Packet', 'ReadPacket', 'WritePacket', 'clientbound', 'send']
+__all__ = ['Packet', 'ReadPacket', 'WritePacket', 'clientbound', 'serverbound']
